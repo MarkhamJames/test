b88aca1fb9d5234428dcc6f0e9a27cfaa8c57dff
James Markham <jamesmarkham@James-Markhams-MacBook-Pro.local>
2019-10-04 13:32:13 -0400

    James Markham final product

--- README.md
+++ README.md
@@ -17,3 +17,5 @@ www.icndb.com is the base URL where the API used in this program "the Internet C
 ## About the Site
 The idea for this website was formulated with the intent to successfully retrieve data from an external API after receiving a command from the user.  One major hurdle that I encountered during the program's construction involved entering a separate first and last name coming from the user input into their respective indices in the API UPL endpoint.  The user enters a string value inside the input bar which needs to be converted into an array.  This array then needs to be split into two indices representing the First and Last name using the '.split' method.  This array then can be used in the URL endpoint.  Font and header changes are utilized for different screen sizes.     
 
+## Features
+
--- app.js
+++ app.js
@@ -1,89 +1,19 @@
 const button = document.querySelector("button")
+const input = document.querySelector("#blank")
 
-button.addEventListener("click", go)
-
-const go = () => {
-  console.log('hello')
-  const readlineSync = require('readline-sync');
+const renderJoke = (funnyjoke) => {
+  let funny = document.querySelector('.jokes')
+  let chuckJoke = document.createElement('div');
+  chuckJoke.innerHTML = `<h1>${funnyjoke}</h1>`
+  console.log(funny)
+  funny.append(chuckJoke)
 }
 
-    function getInput(prompt) {
-      return readlineSync.question(`${prompt}: `);
-      document.getElementById('readline-sync').innerHTML = readlineSync;
-    }
-
-    function greet() {
-      const name = getInput("Welcome! It is a pleasure having you join us. If I may, what is your name?");
-      console.log(name);
-      return name;
-    }
-
-    function compare(card1, card2) {
-      return card1.value - card2.value;
-    }
-
-    function guess(card1, card2) {
-      console.log(card1.rank + card1.suit);
-      const input = getInput("So...what do you think? Will the next card higher (type: h) or lower (type: l)?");
-      if (input === 'h') {
-        return (compare(card1, card2) < 0);
-      } else if (input === 'l') {
-        return (compare(card1, card2) > 0);
-      } else {
-        console.log(`Tsk tsk tsk, you did not type either (h) or (l). No points this round for failing to follow directions. Please try again.`)
-        return false;
-      }
-    }
-    //play game below
-
-    const playGame = async () => {
-      const deck = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
-      console.log(deck)
-      const playerName = greet();
-      const deckId = shuffleNewDeck.data.deck_id
-      let score = 0;
-      let currentCard = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-      console.log(currentCard)
-      const showPlayersCards = (cards) => {
-
-        cards.forEach((card) => {
-          let playerCardChosen = document.createElement('div');
-          playerCardChosen.innerHTML = `<img src=${card.image} />`
-          playersCards.append(playerCardChosen)
-        })
-      }
-      showPlayersCards(currentCard.data.cards)
-      playersCards.style.backgroundImage = `url(${cards[i].image})`
-
-      while (score < 5 && score < deck.length) {
-        let nextCard = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-        console.log(nextCard)
-        const showOpponentsCards = (cards) => {
-          let opponentsCards = document.querySelector('.opponents-cards')
-          cards.forEach((card) => {
-            let opponentsCardChosen = document.createElement('div');
-            opponentsCardChosen.innerHTML = `<img src=${card.image} />`
-            opponentsCards.append(opponentsCardChosen)
-          })
-        }
-        showOpponentsCards(nextCard.data.cards)
-        opponentsCards.style.backgroundImage = `url(${cards[i].image})`
-
-        if (guess(currentCard, nextCard)) {
-          score += 1;
-          console.log(`Congratulations! You scored a point. Your total score is now ${score}!`);
-        } else {
-          console.log(`Sorry, no points for you. Your total score is still ${score}`)
-        };
-        currentCard = nextCard;
-      }
-      deck.length !== 0 ? console.log("Nice job! You win!") : console.log("Oh no! You're out of cards and out of luck. You have lost :(");
-    }
-
-
-
-
-
-
-
-  }    
+button.addEventListener("click", async function () {
+  let array = input.value
+  let newArray = array.split(' ')
+  console.log(newArray)
+  const respond = await axios.get(`https://api.icndb.com/jokes/random?firstName=${newArray[0]}&lastName=${newArray[1]}`)
+  console.log(respond)
+  renderJoke(respond.data.value.joke)
+})
